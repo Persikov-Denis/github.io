c1a44c5d248643718ea0058ba65e1c9ce34bac4e
denispersikov <persikov111222333@gmail.com>
2021-10-01 19:17:11 +0300

    e

--- lucid/www/index.js
+++
@@ -1,7 +0,0 @@
-let menumediatablet = document.querySelector('.menu__media-tablet');
-menumediatablet.addEventListener('click', menu);
-
-function menu() {
-    let tabletblock = document.querySelector('.menu__media-tablet-block');
-    tabletblock.classList.toggle('hidden');
-}
